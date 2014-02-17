fc556085f13d7a319653a2d348532e6e3a941674
Sergio Díaz <sendorf@gmail.com>
2014-02-17 23:08:36 +0100

    Solved task1

--- jobs_manager.rb
+++ jobs_manager.rb
@@ -2,7 +2,12 @@
 class Jobs_Manager
 
   def task0(jobs_sequence)
-    jobs_sequence # Returns the String given as parameter, as long as the string given as parameter is empty 
-                  # the result is the expected.
+    jobs_sequence 	# Returns the String given as parameter, as long as the string given as parameter is empty 
+                  	# the result is the expected.
+  end
+
+   def task1(jobs_sequence)
+    jobs_sequence.chars.sort.join 	# Returns the String given as parameter, as long as the order does not 
+                  					# matter I have decide to order them alphabetically
   end
 end
