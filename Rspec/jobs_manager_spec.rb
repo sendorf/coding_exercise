cc302ec615f031b7dd02f1f5441c99517da3e8ee
Sergio Díaz <sendorf@gmail.com>
2014-02-17 23:15:21 +0100

    Created the spec for task 2

--- Rspec/jobs_manager_spec.rb
+++ Rspec/jobs_manager_spec.rb
@@ -2,15 +2,22 @@
 require_relative '../jobs_manager.rb'
 
 describe Jobs_Manager, "#task 0" do
-  it " The result should be a sequence consisting of a single job a." do
+  it "The result should be a sequence consisting of a single job a." do
     jobs_manager = Jobs_Manager.new
     jobs_manager.task0("").should eq("")
   end
 end
 
 describe Jobs_Manager, "#task 1" do
-  it " The result should be a sequence containing all three jobs abc in no significant order." do
+  it "The result should be a sequence containing all three jobs abc in no significant order." do
     jobs_manager = Jobs_Manager.new
     jobs_manager.task1("cab").should eq("abc")
+  end
+end
+
+describe Jobs_Manager, "#task 2" do
+  it "The result should be a sequence that positions c before b, containing all three jobs abc." do
+    jobs_manager = Jobs_Manager.new
+    jobs_manager.task2("cab").should eq("acb")
   end
 end
